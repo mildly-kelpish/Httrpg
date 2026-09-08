1d36dd17bd221825a54c75e11318364fc08dbdb4
kelp <mildly_kelpish@proton.me>
2026-09-07 21:36:01 -0400

    nearly finished!!

--- client/main.cpp
+++ client/main.cpp
@@ -54,6 +54,7 @@ int main(int argc,char* argv[] ) {
 
     httplib::Client clie(svadress);
     cout << "connecting to server..." << std::endl; // why printf when cout << "string" << std::endl;
+    int playerholder; // only gets used on ptype 2
     nlohmann::json authentication = {{"key", pkey}, {"typ", ptype}};
     cout << authentication.dump() << std::endl;
     auto res = clie.Post("/authenticate", authentication.dump(), "application/json");
@@ -61,6 +62,7 @@ int main(int argc,char* argv[] ) {
          cout << "loading complete!" << std::endl;
          atoken = res->get_header_value("set-cookie");
          cout << res->body << std::endl;
+         playerholder = nlohmann::json::parse(res->body).at("auth");
          clie.set_default_headers({
             {"Cookie", atoken}
          });
@@ -96,16 +98,45 @@ int main(int argc,char* argv[] ) {
         cout << "1: inspect" << "\n" << "2: message" <<"\n"<<"3: modify map" << "\n" << "4: create new object" << "\n" << "5: roll dice" << "\n" << "6: exit" << std::endl;
         cin >> choice;
         switch (choice) { 
-          case 1:
-            cout << "please input object id to inspect" << std::endl;
+          case 1: // inspect
+            if (!args.getBool("verbose")) {
+                cout << "\E[H\E[2J";
+            } // i could do this much better looking if i had a way to "press enter to continue..."   but i dont so 
+            cout << "please input object id to inspect (or \"msgl\")" << std::endl;
             cin >> objid;
-            res = clie.Get("/info/" + objid);
-            cout << res->body << std::endl;
+            if (objid == "msgl") {
+              res = clie.Get("/msg");
+              cout << res->body << std::endl;
+              printloop(map);
+            } else {
+              res = clie.Get("/info/" + objid);
+              printloop(map);
+              cout << objid << ": " << res->body << std::endl;
+            }
             break;
-          case 2:
-            cout << "WIP" << std::endl;
+          case 2: // message
+            if (!args.getBool("verbose")) {
+              cout << "\E[H\E[2J";
+            }
+            cout << "please input message to send (or \"CLR\" to clear messagelog)" << std::endl;
+            cin >> objid;
+            if (objid == "CLR") {
+              res = clie.Get("/msg/clear");
+              cout << res->body << std::endl
+            } else {
+              cout << "please input name to send message under" << std::endl;
+              cin >> objn;
+              objectmake = {{"playerID", 0}, {"undername", objn}, {"content", objid}};
+              res = clie.Post("/msg", objectmake.dump(), "application.json")
+              cout << res->status << std::endl;
+              
+            }
+            printloop(map)
             break;
-          case 4: // i dont know how this happened to get mixed up, but i dont care enough to fix it
+          case 4: // create object
+            if (!args.getBool("verbose")) {
+              cout << "\E[H\E[2J";
+            }
             cout << "please input object id to create" << std::endl;
             cin >> objid;
             cout << "please input object name" << std::endl;
@@ -115,26 +146,30 @@ int main(int argc,char* argv[] ) {
             objectmake = {{"names", objn}, {"status", objstats}};
             res = clie.Post("/make/" + objid, objectmake.dump(), "application/json");
             cout << res->status << std::endl;
+            printloop(map);
             break;
-          case 6:
+          case 6: // exit
             cout << "exiting..." << std::endl;
             exiting = 1;
             break;
-          case 5:
-            cout << "enter dice to roll (must be in XdX format, d must be lowercase)" << std::endl;
+          case 5: // roll
+            if (!args.getBool("verbose")) {
+              cout << "\E[H\E[2J";
+            }
+            cout << "enter dice to roll (must be in XdY format, d must be lowercase)" << std::endl;
             cin >> objid;
             res = clie.Get("/roll/" + objid);
-            cout << res->body << std::endl;
+            printloop(map);
+            cout << "rolled: " <<res->body << std::endl;
+            break;
+          case 3:
+              
 
 
 
 
 
         }
-        if (!args.getBool("verbose")) {
-          cout << "\E[H\E[2J";
-        }
-        printloop(map);
       }
 
     }
@@ -144,20 +179,44 @@ int main(int argc,char* argv[] ) {
         cin >> choice;
         switch (choice) {
           case 1:
+            if (!args.getBool("verbose")) {
+              cout << "\E[H\E[2J";
+            }
             cout << "please input object id to inspect (or \"msgl\")" << std::endl;
             cin >> objid;
             if (objid == "msgl") {
               cout << "reading messagelog" << std::endl;
-              res = clie.Get("/info/");
+              res = clie.Get("/msg");
+              cout << res->body << std::endl;
             } else {
               res = clie.Get("/info/" + objid);
+              printloop(map);
               cout << res->body << std::endl;
             }
             break;
           case 2:
+            if (!args.getBool("verbose")) {
+              cout << "\E[H\E[2J";
+            }
             cout << "please input message to send" << std::endl;
-            cin >> objid; 
+            cin >> objid; // i really just be using objid for everything
+            objectmake = {{"playerID", playerholder}, {"undername", "none"}, {"content", objid}};
+            res = clie.Post("/msg", objectmake.dump(), "application/json");
+            cout << res->status << std::endl;
+            printloop(map);
+            break;
           case 3:
+            if (!args.getBool("verbose")) {
+              cout << "\E[H\E[2J";
+            }
+            cout << "enter dice to roll (must be in XdY format, d must be lowercase)" << std::endl;
+            cin >> objid;
+            res = clie.Get("/roll"+ objid);
+            printloop("map");
+            cout << "rolled: " << res->body << std::endl;
+
+
+          case 4:
             cout << "exiting..." << std::endl;
             exiting = 1;
             break;
